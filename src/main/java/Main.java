8c55f60416948602a4400f5305f8305ef5d12e41
jofravimusic <jofravimusic@byui.edu>
2021-01-30 15:10:48 -0300

    Hi, Austin! It's José Aguirre here. I've added a line of code to your project!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,10 @@
  */
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello my name is Austin Earl");
+        //Hi Austin, José Aguirre Here adding a line of code
+        System.out.println("Hello Austin, my name is José Aguirre.");
     }
 
 }
